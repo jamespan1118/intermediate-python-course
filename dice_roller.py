a511b09ccff874f7e8711da58da244318422dbb1
James Pan <you@example.com>
2020-04-17 12:52:46 +0800

    Dice rolling competition

--- dice_roller.py
+++ dice_roller.py
@@ -1,19 +1,40 @@
 def main():
   import random
+  print("To play the game, please divide the group into two teams")
+  team_1_name = input("Please give the first team a name")
+  team_2_name = input("Please give the second team a name")
   dice_rolls = int(input('How many dice would you like to roll? '))
   dice_size = int(input('How many sides are the dice? '))
-  dice_sum = 0
+  dice_sum_team_1 = 0
+  dice_sum_team_2 = 0
   for i in range(0,dice_rolls):
     roll = random.randint(1,dice_size)
-    dice_sum += roll
+    dice_sum_team_1 += roll
     if roll == 1:
-      print(f'You rolled a {roll}! Critical Fail')
+      print(f'{team_1_name}:You rolled a {roll}! Critical Fail')
     elif roll == dice_size:
-      print(f'You rolled a {roll}! Critical Success!')
+      print(f'{team_1_name}:You rolled a {roll}! Critical Success!')
     else:
-      print(f'You rolled a {roll}')
-  print(f'You have rolled a total of {dice_sum}')
-  print(f'You rolled a total of {dice_sum}')
+      print(f'{team_1_name}:You rolled a {roll}')
+    print(f'{team_1_name}:You have rolled a total of {dice_sum_team_1}')
+
+  for i in range(0,dice_rolls):
+    roll = random.randint(1,dice_size)
+    dice_sum_team_2 += roll
+    if roll == 1:
+      print(f'{team_2_name}:You rolled a {roll}! Critical Fail')
+    elif roll == dice_size:
+      print(f'{team_2_name}:You rolled a {roll}! Critical Success!')
+    else:
+      print(f'{team_2_name}:You rolled a {roll}')
+    print(f'{team_2_name}:You have rolled a total of {dice_sum_team_2}')
+
+  if dice_sum_team_1 > dice_sum_team_2:
+    print(f"Congratualations to {team_1_name}!")
+  elif dice_sum_team_1 < dice_sum_team_2:
+    print(f"Congratualations to {team_2_name}!")
+  else:
+    print("Tie! Play again!")
 
 if __name__== "__main__":
   main()
